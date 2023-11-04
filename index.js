febe4f239dd77af86d91bdcfb7dcbe80b72296f2
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-03 23:27:46 -0400

    workingOnLab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0 ,2)
+}
+// Code your solution in this file!
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(- 2);   
+}
+// Code your solution in this file!
+const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]
+
+// Code your solution in this file!
+
+
