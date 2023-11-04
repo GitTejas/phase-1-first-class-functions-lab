d32fb95af4572032ddda7378fa51f4f5f2569441
Tejas Nampalli <TSNampalli@gmail.com>
2023-11-04 14:10:30 -0400

    finished lab

--- index.js
+++ index.js
@@ -13,4 +13,25 @@ const selectingDrivers = [returnFirstTwoDrivers , returnLastTwoDrivers]
 
 // Code your solution in this file!
 
+const createFareMultiplier = function(int) {
+    return function (fare) {
+        return int * fare 
+    }
+}
+// Code your solution in this file!
 
+const fareDoubler = function(fare) {
+    return fare * 2
+}
+
+// Code your solution in this file!
+
+const fareTripler = function (fare) {
+    return fare *3
+}
+
+// Code your solution in this file!
+const selectDifferentDrivers = function(arrayOfDrivers, yes) {
+    return yes(arrayOfDrivers)
+}
+selectDifferentDrivers()
